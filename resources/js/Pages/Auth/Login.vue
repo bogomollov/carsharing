9a061dba5187d1e72ca9163d38cdbfcb12499796
bogomollov <bogdan.bogomolov.2016@mail.ru>
2026-07-18 18:53:53 +0500

    style: fix label and input colors across themes

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -72,7 +72,7 @@ const submit = () => {
             <div class="block mt-4">
                 <label class="flex items-center">
                     <Checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ms-2 text-sm text-gray-700">Запомнить меня</span>
+                    <span class="ms-2 text-sm text-(--color-text)">Запомнить меня</span>
                 </label>
             </div>
 
